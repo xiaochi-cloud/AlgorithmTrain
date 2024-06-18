a06345b23615bb0b864dc819ef59006f819cb748
xiaochi <zzy345528@cainiao.com>
2024-06-18 22:36:43 +0800

    :feature: 算法-三数之和

--- src/arrays/ThreeSum.java
+++ src/arrays/ThreeSum.java
@@ -10,8 +10,8 @@ import java.util.Set;
 
 /**
  * @Author 霄池
- * @Date 2024/6/11 22:57 <a
- * href="https://leetcode.cn/problems/3sum/solutions/115585/san-shu-zhi-he-javajian-ji-ti-jie-by-wang-zi-hao-z/">题解</a>
+ * @Date 2024/6/11 22:57
+ * <a href="https://leetcode.cn/problems/3sum/solutions/115585/san-shu-zhi-he-javajian-ji-ti-jie-by-wang-zi-hao-z/">题解</a>
  */
 public class ThreeSum {
     //给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] +
@@ -22,6 +22,80 @@ public class ThreeSum {
     //注意：答案中不可以包含重复的三元组。
     public List<List<Integer>> threeSum(int[] nums) {
 
+        return fun4(nums);
+    }
+
+    private static ArrayList<List<Integer>> fun4(int[] nums) {
+        Arrays.sort(nums);
+
+        ArrayList<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int numResult = nums[i];
+            if (i > 0 && numResult == nums[i - 1]) {
+                continue;
+            }
+            int head = i + 1, tail = nums.length - 1;
+            while (head < tail) {
+                int r = nums[head] + nums[tail] + nums[i];
+                if (r == 0) {
+                    result.add(Arrays.asList(nums[i], nums[head], nums[tail]));
+
+                    for (++head; head < tail && nums[head] == nums[head - 1]; ++head)
+                        ; // 跳过重复数字
+                    for (--tail; tail > head && nums[tail] == nums[tail + 1]; --tail)
+                        ; // 跳过重复数字
+
+                }
+                if (r < 0) {
+                    head++;
+                }
+                tail--;
+            }
+
+        }
+        return result;
+    }
+
+    /**
+     * 夹逼法
+     * @param nums
+     * @return
+     */
+    private static ArrayList<List<Integer>> fun3(int[] nums) {
+        //1. 排序
+        Arrays.sort(nums);
+
+        LinkedHashSet<List<Integer>> result = new LinkedHashSet<>();
+        //2.夹闭
+        for (int i = 0; i < nums.length - 2; i++) {
+            int head = i + 1;
+            int tail = nums.length - 1;
+            while (head < tail) {
+                int except = -nums[i];
+                int caculatorResult = nums[tail] + nums[head];
+                //跳出条件
+                if (caculatorResult == except) {
+                    result.add(Arrays.asList(nums[i], nums[head], nums[tail]));
+                    tail--;
+                    continue;
+                }
+                if (caculatorResult > except) {
+                    tail--;
+                    continue;
+                }
+                head++;
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    /**
+     * 两重暴力+hash
+     * @param nums
+     * @return
+     */
+    private static ArrayList<List<Integer>> fun1(int[] nums) {
         Set<List<Integer>> result = new LinkedHashSet<>();
         for (int i = 0; i < nums.length; i++) {
             Integer outerExcept = -nums[i];
